197b56fbac4035253ce56d7188c6446eb92d33fb
Deneb.99 <moisacerrato@gmail.com>
2024-02-24 10:09:20 -0600

    Incorporacion de Auth0
    Se agrego la funcionalidad de auth0

---
+++ src/Components/Logoutwithredirect.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Logout = () => {
+    const{logout} = useAuth0()
+    return (
+        <div>
+        <button onClick={() => logout()} className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Cerrar Sesión
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                </button>
+                </div>
+    )
+}
+
+export default Logout;
