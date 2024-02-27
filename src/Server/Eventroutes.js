a3e041c1e0797406b95521d682b45e42cc3b26ae
Deneb.99 <moisacerrato@gmail.com>
2024-02-26 22:12:05 -0600

    Rutas
    Corregido error de server de rutas

--- src/Server/Eventroutes.js
+++ src/Server/Eventroutes.js
@@ -1,30 +1,29 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const router = express.Router();
 // Ruta para crear un nuevo evento (POST)
-app.post('/eventos', (req, res) => {
-  console.log(reqle)
-  res.send('Evento creado con éxito');
+router.get('/eventos', (req, res) => {
+  res.send("Ruta de eventos");
 });
 
 // Ruta para editar un evento existente (PUT/PATCH)
-app.put('/eventos/:id', (req, res) => {
+router.put('/eventos/:id', (req, res) => {
   // Lógica para editar un evento por su ID
   res.send(`Evento con ID ${req.params.id} editado`);
 });
 
 // Ruta para mostrar detalles de un evento (GET)
-app.get('/eventos/:id', (req, res) => {
+router.get('/eventos/:id', (req, res) => {
   // Lógica para obtener detalles de un evento por su ID
   res.send(`Detalles del evento con ID ${req.params.id}`);
 });
 
 // Ruta para eliminar un evento (DELETE)
-app.delete('/eventos/:id', (req, res) => {
+router.delete('/eventos/:id', (req, res) => {
   // Lógica para eliminar un evento por su ID
   res.send(`Evento con ID ${req.params.id} eliminado`);
 });
 
+module.exports = router;
 // Iniciar el servidor
 //app.listen(3000, () => {
  // console.log('Servidor en funcionamiento en el puerto 3000');
--- src/Server/Server.js
+++ src/Server/Server.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
+
 //Lamado para las variables de ambientes
 require("dotenv").config({path: "./db.env"});
 //Constante de conexion
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 //Para encontrar los origenes de conexion
 app.use(cors(port));
 //Para que use las librerias de conexion
 app.use(express.json());
 //Ruta del documento que se va hacer en MongoDB ATLAS
-//app.use(require("./routes/records"));
+//app.use('/', require("./Eventroutes.js"));
 //Conexion al Driver
 const dbo = require("./db.connection.js");
 //Funcion para escuchar y realizar conexion con el driver del cluster
-app.listen(port, () => {
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use('/', require("./Eventroutes.js"));
+
+app.use(express.static(__dirname + "/public"));
+
+
+//app.listen(port, () => {
             //Realiza la conexion a la base de datos
-            dbo.connectToServer(async function (err) {
-                if (err) 
-                    console.error(err);
-             }); 
-             console.log("Conectado a la base de datos");
-        });
+           // dbo.connectToServer(async function (err) {
+                //if (err) 
+                  //  console.error(err);
+             //}); 
+             //console.log("Conectado a la base de datos");
+        //});
+
+        app.listen(port, () => {
+            console.log('servidor a su servicio en el puerto', port)
+        })
