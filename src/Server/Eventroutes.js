d7aae98f323744a7058da43231c584953b860b90
Deneb-99 <160911708+Deneb-99@users.noreply.github.com>
2024-03-05 18:52:02 -0600

    Actualizacion eventos

--- src/Server/Eventroutes.js
+++ src/Server/Eventroutes.js
@@ -8,7 +8,7 @@ app.use(express.json())
 router.post('/crear', async(req, res) => {
   try {
     const product = await Product.create(req.body)
-    res.status(200).json(product);
+    res.status(200).json(Product);
 } catch (error) {
     console.log(error.message)
     res.status(500).json({message: error.message})
