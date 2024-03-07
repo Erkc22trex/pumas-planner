e28b2fd13ef29e0e4663891186a2db775ba3b6f4
Deneb-99 <160911708+Deneb-99@users.noreply.github.com>
2024-03-06 18:23:08 -0600

    Update Eventroutes.js

--- src/Server/Eventroutes.js
+++ src/Server/Eventroutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const Product = require('../Models/Eventmodel')
+const Evento = require('../Models/Eventmodel')
 const app = express()
 
 app.use(express.json())
 // Ruta para crear un nuevo evento (POST)
 router.post('/crear', async(req, res) => {
   try {
-    const product = await Product.create(req.body)
-    res.status(200).json(product);
+    const evento = await Evento.create(req.body)
+    res.status(200).json(evento);
 } catch (error) {
     console.log(error.message)
     res.status(500).json({message: error.message})
@@ -17,26 +17,51 @@ router.post('/crear', async(req, res) => {
 });
 
 // Ruta para filtrar un nuevo evento (POST)
-router.get('/filtrar/:filter', (req, res) => {
-  res.send(`Evento con ID ${req.params.filter} filtrados  `);
+router.get ('/filtrar', async (req, res) => {
+  try {
+      const evento = await Evento.find({});
+      res.status(200).json(evento);
+  } catch (error) {
+      res.status(500).json({message: error, message})
+  }
+});
+router.get('/filtrar/:id', async(req, res) => {
+  try {
+    const {id} = req.params;
+    const evento = await Evento.findById(id);
+    res.status(200).json(evento);
+} catch (error) {
+    res.status(500).json({message: error, message})
+}
 });
 
-// Ruta para editar un evento existente (PUT/PATCH)
-router.put('/editar/:id', (req, res) => {
+// Ruta para editar un comentario existente (PUT/PATCH)
+router.put('/editar/:id', async(req, res) => {
   // Lógica para editar un evento por su ID
-  res.send(`Evento con ID ${req.params.id} editado`);
+  try {
+    const {id} = req.params;
+    const evento = await Evento.findByIdAndUpdate(id, req.body);
+    //No se puede encontrar el producto en la base de datos
+    if (!evento){
+        return res.status(404).json({message: `No se puede encontrar ningun evento con el ID ${id}`});
+    }
+    res.status(200).json(evento);
+}catch (error) {
+    res.status(500).json({message: error, message})
+}
 });
-
-// Ruta para mostrar detalles de un evento (GET)
-router.get('/detalles/:id', (req, res) => {
-  // Lógica para obtener detalles de un evento por su ID
-  res.send(`Detalles del evento con ID ${req.params.id}`);
-});
-
-// Ruta para eliminar un evento (DELETE)
-router.delete('/eliminar/:id', (req, res) => {
+router.delete('/eliminar/:id', async(req, res) => {
   // Lógica para eliminar un evento por su ID
-  res.send(`Evento con ID ${req.params.id} eliminado`);
+  try {
+    const {id} = req.params;
+    const evento = await Evento.findByIdAndDelete(id);
+    if (!evento){
+        return res.status(404).json({message: `No se puede encontrar ningun evento con el ID ${id}`});
+    }
+    res.status(200).json(evento);
+} catch (error) {
+    res.status(500).json({message: error, message})
+}
 });
 
 module.exports = router;
