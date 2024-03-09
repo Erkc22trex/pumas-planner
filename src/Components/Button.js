4ebbf876a6b1dd3e320fb3e7381d66a51a65095f
BJoelValle <bary.valle@unah.hn>
2024-03-09 15:46:02 -0600

    setting

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,32 +1,58 @@
 import { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
 
 export default function DropdownBtn() {
-    const [isActive, SetIsActive] = useState(true)
+    const { isAuthenticated, logout, user } = useAuth0();
+    const [dropdownVisible, setDropdownVisible] = useState(false);
 
-    const handleClick = () => {
-        SetIsActive(!isActive)
+    const toggleDropdown = () => {
+        setDropdownVisible(!dropdownVisible);
     }
 
     return (
         <>
-            <button onClick={handleClick} id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"> dropdown
-                <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                </svg>
-            </button>
-            <div id="dropdownNavbar" class={`${isActive ? 'hidden' : ''} z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}>
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">                   
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                </ul>
-                <div class="py-1">
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                </div>
-            </div>
+            {
+                isAuthenticated && (
+                    <div className='relative inline-block text-left'>
+                        <button id="dropdownAvatarNameButton" data-dropdown-toggle="dropdownAvatarName" className="flex items-center text-sm pe-1 font-medium text-gray-900 rounded-full hover:text-blue-600 dark:hover:text-blue-500 md:me-0 dark:text-white" type="button" onClick={toggleDropdown}>
+                            <span className="sr-only">Open user menu</span>
+                            <span className='text-center px-2'>
+                                {user?.name}                                
+                            </span>
+                            <img className="w-8 h-8 me-2 rounded-full" src={user?.picture || "logo512.png"} alt="user photo" />
+                            {/* <svg className="w-2.5 h-2.5 ms-3" ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                            </svg> */}
+                        </button>
+
+                        {/* <div id="dropdownAvatarName" className={`z-10 ${!dropdownVisible ? 'hidden' : ''} bg-white divide-y divide-gray-700 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}> */}
+                        <div id="dropdownAvatarName" className={`${!dropdownVisible ? 'hidden' : ''} absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white  shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}>
+                            {
+                                dropdownVisible && (
+                                    <>
+                                        <div className="px-4 py-3 text-sm text-gray-900 dark:text-gray-900">
+                                            {/* <div className="font-medium">Pro User</div> */}
+                                            <div className="truncate">{user?.email}</div>
+                                        </div>
+                                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-900" aria-labelledby="dropdownInformdropdownAvatarNameButtonationButton">
+                                            {/* <li>
+                                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                            </li> */}
+                                            <li>
+                                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white">Configuración</a>
+                                            </li>
+                                        </ul>
+                                        <div className="py-2" onClick={() => logout()}>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-900 dark:hover:text-white">Cerrar sesión</a>
+                                        </div>
+                                    </>
+                                )
+                            }
+                        </div>
+                    </div>
+                )
+            }
         </>
-    )
+    );
 }
--- src/Components/HomePage/Navbar.js
+++ src/Components/HomePage/Navbar.js
@@ -4,13 +4,13 @@ import Login from '../Login';
 import Logout from '../Logoutwithredirect';
 import DropdownBtn from '../Button';
 import NotificationButton from './NotificationButton';
-import SearchBar from '../Search'; 
+import SearchBar from '../Search';
 
 export function Navbar() {
     const { isAuthenticated } = useAuth0();
 
     return (
-        <header className="text-gray-00 bg-amber-400 body-font sticky">
+        <header className="text-gray-900 bg-amber-400 body-font sticky">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <div className="flex items-center justify-center md:justify-start w-full md:w-auto mb-4 md:mb-0">
                     <img
@@ -22,17 +22,27 @@ export function Navbar() {
                     </span>
                 </div>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center md:justify-end space-x-6">
-                    {isAuthenticated && <SearchBar />} {/* Muestra la barra de búsqueda solo si el usuario está autenticado */}
-                    {isAuthenticated && <NotificationButton />} {/* Muestra el botón de notificaciones solo si el usuario está autenticado */}
-                    {isAuthenticated ? (
-                        <DropdownBtn />
+                    {
+                        isAuthenticated && (
+                            <>
+                                <SearchBar />
+                                <NotificationButton />
+                            </>
+                        )
+                    }
+                    {/* {isAuthenticated ? (
+                        <>
+                            <NotificationButton />
+                            <SearchBar />
+                        </>
                     ) : (
                         <a className="hover:text-gray-900 border-y-2 border-y-stone-900">
-                        CONTACTOS
+                            CONTACTOS 
                         </a>
-                    )}
+                    )} */}
+                    <DropdownBtn />
                 </nav>
-                {isAuthenticated ? <Logout /> : <Login />}
+                {!isAuthenticated && <Login />}
             </div>
         </header>
     );
