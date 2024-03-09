a2257656ca6cdda2af028e6426e423683159e328
BJoelValle <bary.valle@unah.hn>
2024-03-09 03:55:50 -0600

    Detalles en formulario

--- src/Components/EventPage/Form.js
+++ src/Components/EventPage/Form.js
@@ -2,25 +2,13 @@ import React, { useState } from 'react';
 import Btn from "../Btn.tsx";
 import Inputset from "../Input.tsx";
 import Map from "../EventPage/Map.js";
-import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api'; 
 
-function Form() {
+export default function Form() {
     const [valorDelInput, setValorDelInput] = useState('');
-    const [descripcion, setDescripcion] = useState(''); 
-
-    const [markers, setMarkers] = useState([]); // Estado para almacenar las ubicaciones marcadas
-
-    // Función para manejar el clic en el mapa y agregar un marcador en la ubicación
-    const handleMapClick = (event) => {
-        const newMarker = {
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng()
-        };
-        setMarkers([...markers, newMarker]); 
-    };
+    const [descripcion, setDescripcion] = useState('');
 
     return (
-        
+
         <div className={"bg-gradient-to-r from-[#18012E] via-[#322894] to-[#18012E]"}>
             <h1 className="text-white text-center -mb-6 text-6xl font-semibold title-font ">Creación del Evento</h1>
             <div className="container px-5 py-10 mx-auto flex justify-center sm:flex-nowrap flex-wrap">
@@ -71,27 +59,18 @@ function Form() {
                             onChange={(e) => setDescripcion(e.target.value)}
                         ></textarea>
                     </div>
-                    <LoadScript googleMapsApiKey="AIzaSyDfVj-Cnt4b0MzOoCGGFqCY3zhymkPEUSY">
-                        <GoogleMap
-                            mapContainerStyle={{ height: '400px', width: '100%' }}
-                            center={{ lat: 14.084818012234798, lng: -87.16664545886665 }} // Coordenadas de Tegucigalpa como centro del mapa
-                            zoom={10}
-                            onClick={handleMapClick} // Maneja el clic en el mapa para agregar marcadores
-                        >
-                            {/* Renderiza todos los marcadores en el mapa */}
-                            {markers.map((marker, index) => (
-                                <Marker key={index} position={marker} />
-                            ))}
-                        </GoogleMap>
-                    </LoadScript>
-                    <div className="flex items-center justify-center w-full my-3">
-                        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+
+                    <Map />
+
+                    <div class="flex items-center
+                     justify-center w-full mt-5 mb-5">
+                        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                                 </svg>
-                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                <p className="mb-2 text-sm text-gray-00 dark:text-gray-400"><span className="font-semibold text-xl">Click para subir o arrastrar y soltar</span></p>
+                                <p className="text-base text-gray-500 dark:text-gray-400">SVG, PNG, JPG, GIF or MP4 </p>
                             </div>
                             <input id="dropzone-file" type="file" className="hidden" />
                         </label>
@@ -106,4 +85,3 @@ function Form() {
     );
 }
 
-export default Form;
