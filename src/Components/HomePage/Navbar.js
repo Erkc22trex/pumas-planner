22b5c2691affe1982ff0dd39d91c5a1059bf8829
aeherreraf <aaronherrera985@gmail.com>
2024-03-09 04:22:33 -0600

    Notificaciones

--- src/Components/HomePage/Navbar.js
+++ src/Components/HomePage/Navbar.js
@@ -7,12 +7,12 @@ import NotificationButton from './NotificationButton';
 import SearchBar from '../Search'; 
 
 export function Navbar() {
-    const { isAuthenticated, isLoading } = useAuth0();
+    const { isAuthenticated } = useAuth0();
 
     return (
         <header className="text-gray-00 bg-amber-400 body-font sticky">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <div className="flex items-center justify-start w-full md:w-auto mb-4 md:mb-0">
+                <div className="flex items-center justify-center md:justify-start w-full md:w-auto mb-4 md:mb-0">
                     <img
                         src="./puma2.png"
                         className="w-12 h-16 text-white p-2 bg-transparent rounded-full"
@@ -21,7 +21,7 @@ export function Navbar() {
                         PUMAS PLANNER
                     </span>
                 </div>
-                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center space-x-6">
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center md:justify-end space-x-6">
                     {isAuthenticated && <SearchBar />} {/* Muestra la barra de búsqueda solo si el usuario está autenticado */}
                     {isAuthenticated && <NotificationButton />} {/* Muestra el botón de notificaciones solo si el usuario está autenticado */}
                     {isAuthenticated ? (
--- src/Components/HomePage/NotificationButton.js
+++ src/Components/HomePage/NotificationButton.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function NotificationButton() {
   const [notificationCount, setNotificationCount] = useState(0);
   const [notifications, setNotifications] = useState([]);
   const [showNotifications, setShowNotifications] = useState(false);
+
+  const notificationsRef = useRef(null);
 
   // Función para cargar las notificaciones desde la API
   const fetchNotifications = async () => {
@@ -27,8 +29,22 @@ export default function NotificationButton() {
     setShowNotifications(!showNotifications);
   };
 
+  // Manejador de eventos de clic en el documento para cerrar la bandeja al hacer clic fuera de ella
+  const handleClickOutside = (event) => {
+    if (notificationsRef.current && !notificationsRef.current.contains(event.target)) {
+      setShowNotifications(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="relative mr-4">
+    <div className="relative mr-4" ref={notificationsRef}>
       <button className="w-8 h-8 text-white" onClick={toggleNotifications}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.585 15.5H5.415A1.65 1.65 0 0 1 4 13a10.526 10.526 0 0 0 1.5-5.415V6.5a4 4 0 0 1 4-4h2a4 4 0 0 1 4 4v1.085c0 1.907.518 3.78 1.5 5.415a1.65 1.65 0 0 1-1.415 2.5zm1.915-11c-.267-.934-.6-1.6-1-2s-1.066-.733-2-1m-10.912 3c.209-.934.512-1.6.912-2s1.096-.733 2.088-1M13 17c-.667 1-1.5 1.5-2.5 1.5S8.667 18 8 17"/>
@@ -60,5 +76,6 @@ export default function NotificationButton() {
     </div>
   );
 }
+
 
 
