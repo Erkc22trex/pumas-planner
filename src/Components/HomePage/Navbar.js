51a46b9c7ede3b013394c97e03afea28a044b0f4
aeherreraf <aaronherrera985@gmail.com>
2024-03-09 04:03:03 -0600

    cambios en navbar

--- src/Components/HomePage/Navbar.js
+++ src/Components/HomePage/Navbar.js
@@ -1,16 +1,18 @@
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Login from '../Login';
 import Logout from '../Logoutwithredirect';
 import DropdownBtn from '../Button';
-import SearchBtn from '../Search';    
+import NotificationButton from './NotificationButton';
+import SearchBar from '../Search'; 
+
 export function Navbar() {
-    const { isAuthenticated, isLoading } = useAuth0()
-    
+    const { isAuthenticated, isLoading } = useAuth0();
 
     return (
         <header className="text-gray-00 bg-amber-400 body-font sticky">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <div className="flex items-center justify-start w-full md:w-auto mb-4 md:mb-0">
                     <img
                         src="./puma2.png"
                         className="w-12 h-16 text-white p-2 bg-transparent rounded-full"
@@ -18,23 +20,17 @@ export function Navbar() {
                     <span className="ml-3 text-2xl text-white font-bold">
                         PUMAS PLANNER
                     </span>
-                </a>
-                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    {
-                        isAuthenticated ?
-                            (
-                                <div>
-                                    <SearchBtn />
-                                    <DropdownBtn />
-                                </div>
-                            ) : (
-                                <>
-                                    <a className="mr-5 hover:text-gray-900 border-y-2 border-y-stone-900">
-                                        Contacto
-                                    </a>
-                                </>
-                            )
-                    }
+                </div>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center space-x-6">
+                    {isAuthenticated && <SearchBar />} {/* Muestra la barra de búsqueda solo si el usuario está autenticado */}
+                    {isAuthenticated && <NotificationButton />} {/* Muestra el botón de notificaciones solo si el usuario está autenticado */}
+                    {isAuthenticated ? (
+                        <DropdownBtn />
+                    ) : (
+                        <a className="hover:text-gray-900 border-y-2 border-y-stone-900">
+                        CONTACTOS
+                        </a>
+                    )}
                 </nav>
                 {isAuthenticated ? <Logout /> : <Login />}
             </div>
